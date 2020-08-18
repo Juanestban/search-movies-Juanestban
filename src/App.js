a6bbbf5eaff2037a9d547865954513526486527f
juanestban <53704567+Juanestban@users.noreply.github.com>
2020-08-18 17:05:54 -0500

    inicializando el 'Search Movies'
    Buscador de Peliculas online

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bulma/css/bulma.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// components
+import { Title } from './components/Title';
+import { SearchForm } from './components/SearchForm';
+
+class App extends Component {
+  state = { results: [] }
+
+  handleChange = (results) => {
+    this.setState({ results });
+  }
+
+  renderResult() {
+    const { results } = this.state;
+    return results.map((movie) => {
+      return (
+        <div
+          key={movie.imdbID}
+          className="moviesContainar"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <p
+            style={{ padding: 10, marginTop: 10 }}
+          >
+            {movie.Title}
+          </p>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>
+          Search Movies
+        </Title>
+        <div className="SeachForm-wrapper">
+          <SearchForm onResult={this.handleChange} />
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin Resultados</p>
+          : this.renderResult()
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+
+const API_KEY = 'd45ac7f8';
+
+export class SearchForm extends Component {
+    state = {
+        inputMovie: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({ inputMovie: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { inputMovie } = this.state;
+
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+            .then((res) => res.json())
+            .then((data) => {
+                const { Search, totalResults } = data;
+                console.log({ Search, totalResults});
+                this.props.onResult(Search);
+            });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="field has-addons">
+                    <div className="control">
+                        <input
+                            className="input"
+                            onChange={this.handleChange}
+                            type="text"
+                            placeholder="Movies to search..." />
+                    </div>
+                    <div className="control">
+                        <button className="button is-info">
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
